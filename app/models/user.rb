18fa54868598e26ba77d3c72fe7249c92dbc5607
ngs0908 <muneyuki9296@gmail.com>
2023-06-21 10:31:55 +0900

    エラーメッセージとバリテーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :date_of_birth,  presence: true
+  validates :nickname,  :date_of_birth,  presence: true
+  validates_format_of :last_name, :first_name, with: /\A[ぁ-んァ-ン一-龥]/
+  validates_format_of :last_name_kana, :first_name_kana, with: /\A[ァ-ヶー－]+\z/
 end

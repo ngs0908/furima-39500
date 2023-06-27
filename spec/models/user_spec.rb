ff52ec5399864da8df5c37957d8966b20b08644e
ngs0908 <muneyuki9296@gmail.com>
2023-06-27 16:58:05 +0900

    ユーザー管理機能実装【修正②】

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
+      it "入力条件を満たせば登録できる" do
+      expect(@user).to be_valid
+      end
+    end
+
     context '新規登録できない場合' do
       it "nicknameが空では登録できない" do
         @user.nickname = ''

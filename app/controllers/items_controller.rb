1bf2ae72ee6cecabd1627ab03c7cea0500b3397a
ngs0908 <muneyuki9296@gmail.com>
2023-06-28 17:54:22 +0900

    出品可能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,25 @@
 class ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
+  
   def index
     
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item = current_user.items.build(item_params)
+    if @item.save
+      redirect_to @item, notice: 'Item was successfully created.'
+    else
+      render :new
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :ship_date_id, :price)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+ 
   root 'items#index'
   resources :users
   resources :items
 
   resources :articles
+
+  # post '/items/new', to: 'items#create'
 end

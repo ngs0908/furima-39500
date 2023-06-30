e607caa6137103ee9d2a1acf857219dc396542b0
ngs0908 <muneyuki9296@gmail.com>
2023-06-30 19:14:21 +0900

    商品出品機能 【修正②】

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,10 @@
 class Item < ApplicationRecord
-  has_one :purchase
+  # has_one :purchase
+  include ActiveHash::Associations
   belongs_to :user
   has_one_attached :image
 
-  validates :image, :item_name, :description, :price, presence: true
+  validates :image, :item_name, :description, presence: true
   validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :ship_date_id, exclusion: { in: [1], message: "is not a valid option" }
   validates :price, presence: true,
                     numericality: {

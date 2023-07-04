eda13a47464fecfeda0a36ecd6bd131b0aa6fc33
ngs0908 <muneyuki9296@gmail.com>
2023-07-04 14:20:33 +0900

    商品出品機能 【修正③-3】

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,12 @@
 class Item < ApplicationRecord
   # has_one :purchase
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :user, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :ship_date_id,
+  belongs_to :user
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :ship_date
   has_one_attached :image
 
   validates :image, :item_name, :description, presence: true

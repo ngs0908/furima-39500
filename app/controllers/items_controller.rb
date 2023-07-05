17929e5e77356dfcaaddc90a545e9cbefdf9fa99
ngs0908 <muneyuki9296@gmail.com>
2023-07-05 14:37:38 +0900

    商品情報編集機能 実装【修正①-2】

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit,]
   before_action :set_item, only: [:show, :edit, :update]
 
+  
   def new
     @item = Item.new
   end

51f9789d94c0955d21324b09d6b8915749b73af3
ngs0908 <muneyuki9296@gmail.com>
2023-07-05 14:29:26 +0900

    商品情報編集機能 実装【修正①】

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit,]
+  before_action :set_item, only: [:show, :edit, :update]
+
   def new
     @item = Item.new
   end
@@ -18,23 +20,24 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
     @user = @item.user
   end
 
   def edit
-    @item = Item.find(params[:id])
     return unless current_user != @item.user
     redirect_to root_path
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to @item
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   private

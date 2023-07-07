f383722b57925d0a7133b3dea21da4f9126faccf
ngs0908 <muneyuki9296@gmail.com>
2023-07-07 18:51:55 +0900

    device 再作成

--- db/schema.rb
+++ db/schema.rb
@@ -52,21 +52,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_05_073247) do
     t.index ["purchase_id"], name: "index_addresses_on_purchase_id"
   end
 
-  create_table "categories", charset: "utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "genre_id", null: false
-  end
-
-  create_table "conditions", charset: "utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "genre_id", null: false
-  end
-
-  create_table "delivery_charges", charset: "utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "genre_id", null: false
-  end
-
   create_table "items", charset: "utf8", force: :cascade do |t|
     t.string "item_name", null: false
     t.text "description", null: false
@@ -82,11 +67,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_05_073247) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
-  create_table "prefectures", charset: "utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "genre_id", null: false
-  end
-
   create_table "purchases", charset: "utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "item_id", null: false
@@ -96,25 +76,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_05_073247) do
     t.index ["user_id"], name: "index_purchases_on_user_id"
   end
 
-  create_table "ship_dates", charset: "utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "genre_id", null: false
-  end
-
-  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+  create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nickname", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.date "date_of_birth", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "nickname"
-    t.string "last_name"
-    t.string "first_name"
-    t.string "last_name_kana"
-    t.string "first_name_kana"
-    t.date "date_of_birth"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

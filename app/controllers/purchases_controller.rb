2fb3b1ff1e0833f1fe7a2e7f2b83acf315462a1f
ngs0908 <muneyuki9296@gmail.com>
2023-07-07 15:01:47 +0900

    商品情報編集機能 実装 【仮2 】

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,6 +4,10 @@ class PurchasesController < ApplicationController
   def index
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @purchase_address = PurchaseAddress.new
+    
+    if @item.sold_out?
+      redirect_to root_path
+    end
   end
 
   def set_item
@@ -18,12 +22,13 @@ class PurchasesController < ApplicationController
       @purchase_address.save
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
 
   def purchase_address_params
-    params.require(:purchase_address).permit(:post_code, :prefecture_id, :municipality, :house_number, :house_name, :phone_number, :token).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_address).permit(:post_code, :prefecture_id, :municipality, :house_number, :house_name, :phone_number, :token).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   private
@@ -31,14 +36,14 @@ class PurchasesController < ApplicationController
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
       Payjp::Charge.create(
-        # amount: @item.price,  # 商品の値段
+        amount: @item.price,  # 商品の値段
         card: purchase_address_params[:token],    # カードトークン
         currency: 'jpy'                 # 通貨の種類（日本円）
       )
     end
 
-  def purchase_params
-    params.require(:purchase_address).permit(:post_code, :prefecture_id, :municipality, :address, :house_number, :house_name, :phone_number, :user_id).merge(user_id: current_user.id, token: params[:token])
-  end
+  # def purchase_params
+  #   params.require(:purchase_address).permit(:post_code, :prefecture_id, :municipality, :address, :house_number, :house_name, :phone_number, :user_id).merge(user_id: current_user.id, token: params[:token])
+  # end
 
 end
